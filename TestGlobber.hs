ddd3ab7c94fcd9f89f1b7682fb44dd188755cf9a
xxu52 <xxu52@mir.cs.illinois.edu>
2016-05-18 20:53:10 -0500

    Add more tests

--- TestGlobber.hs
+++ TestGlobber.hs
@@ -13,3 +13,34 @@ main = hspec $ describe "Testing Globber" $ do
       it "shouldn't match non-empty string" $
         matchGlob "" "string" `shouldBe` False
 
+    describe "literal match" $ do
+      it "matches single character" $
+        matchGlob "a" "a" `shouldBe` True
+      it "should not match wrong character" $
+        matchGlob "a" "b" `shouldBe` False
+      it "should not match empty string" $
+        matchGlob "a" "" `shouldBe` False
+      it "should not match empty pattern" $
+        matchGlob "" "a" `shouldBe` False
+      it "should match string" $
+        matchGlob "abcd" "abcd" `shouldBe` True
+      it "should not match wrong string" $
+        matchGlob "abcd" "ebcd" `shouldBe` False
+      it "should not match wrong string" $
+        matchGlob "abcd" "abce" `shouldBe` False
+      it "should not match string of wrong length" $
+        matchGlob "abcd" "abcde" `shouldBe` False
+      it "should not match string of wrong length" $
+        matchGlob "abcde" "abcd" `shouldBe` False
+      it "should not match string of wrong length" $
+        matchGlob "bcd" "abcd" `shouldBe` False
+      it "should not match string of wrong length" $
+        matchGlob "abcd" "bcd" `shouldBe` False
+    
+    describe "? match" $ do
+      it "should match single character" $
+        matchGlob "?" "a" `shouldBe` True
+      it "should match string" $
+        matchGlob "a?c" "abc" `shouldBe` True
+      it "should not match wrong length" $
+        matchGlob "ab?c" "abc" `shouldBe` False
